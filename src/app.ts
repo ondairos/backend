77e80c0755c63b57711e4c857da75af4d0e826b1
Ondairos <ondairos@gmail.com>
2024-02-28 15:45:59 +0200

    added backend houses api

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import { housesData } from "./rawData";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/houses", (req: Request, res: Response) => {
+  if (!housesData) {
+    res.json({ error: "Houses not found!" });
+  }
+  const name = req.query.name as string;
+
+  if (name) {
+    const house = housesData.find(
+      (foundHouse) => foundHouse.name.toLowerCase() === name.toLowerCase()
+    );
+    res.json(house);
+  } else {
+    res.json(housesData);
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
